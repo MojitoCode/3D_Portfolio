9b1cd062f67304fb75b05d00876ab8c9f378fc27
ZomB37 <HowlingNights@gmail.com>
2024-08-15 22:10:08 -0700

    updated send function

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -13,7 +13,8 @@ const Contact = () => {
   const handleSubmit = (e) => {
     e.preventDefault();
     setIsLoading(true);
-    emailjs.sendForm(
+
+    emailjs.send(
       import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
       import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
       {
