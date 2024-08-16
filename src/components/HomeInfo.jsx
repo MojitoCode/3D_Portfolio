48e08cf5d859b5e2fc71626406397e0a9d202ac2
ZomB37 <HowlingNights@gmail.com>
2024-08-15 20:17:31 -0700

    daily backup; implemented infobox links

--- src/components/HomeInfo.jsx
+++ src/components/HomeInfo.jsx
@@ -1,30 +1,33 @@
-import React from 'react'
-
-const renderContent = {
-    1: (
-        <h1 className="sm:text-xl sm:leading-snug text-center neo-brutalism-blue py-4 px-8 text-white mx-5">Hi, I'm <span className="font-semibold">Sam</span> 👋🏻
-        <br></br>
-        a Software Developer from the U.S.</h1>
-    ),
-    2: (
-        <h1>2</h1>
-    ),
-    3: (
-        <h1>3</h1>
-    ),
-    4: (
-        <h1>4</h1>
-    ),
-}
+import { Link } from "react-router-dom";
+import { arrow } from "../assets/icons";
 
 const InfoBox = ({ text, link, btnText }) => (
-    <div className='info-box'>
-        {text}
-    </div>
-)
+  <div className="info-box">
+    <p className="font-medium sm:text-xl text-center ">{text}</p>
+    <Link to={link} className="neo-brutalism-white neo-btn">
+      {btnText}
+      <img src={arrow} className="w-4 h-4 object-contain" />
+    </Link>
+  </div>
+);
+
+const renderContent = {
+  1: (
+    <h1
+      className="sm:text-xl sm:leading-snug text-center 
+        neo-brutalism-blue py-4 px-8 text-white mx-5"
+    >
+      Hi, I'm <span className="font-semibold">Sam</span>👋🏻
+      <br />a Software Developer from the U.S.
+    </h1>
+  ),
+  2: <InfoBox text="Transforming Code into Creativity, One Line at a Time." link="/about" btnText="Learn more" />,
+  3: <InfoBox text="Your Vision, My Expertise" link="/projects" btnText="Visit My Portfolio" />,
+  4: <InfoBox text="Ready to Collaborate?" link="/contact" btnText="Let's Chat" />,
+};
 
 const HomeInfo = ({ currentStage }) => {
   return renderContent[currentStage] || null;
-}
+};
 
-export default HomeInfo
+export default HomeInfo;
