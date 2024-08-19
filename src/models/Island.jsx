2145cb4eaec4eeca94aa4a318791ffbc963428cb
ZomB37 <HowlingNights@gmail.com>
2024-08-18 21:08:22 -0700

    finished about section and linked contact form
    fixed the island rotation animation/speed when navigating with left/right arrow keys
    
    workin on project page next

--- src/models/Island.jsx
+++ src/models/Island.jsx
@@ -49,15 +49,17 @@ const Island = ({isRotating, setIsRotating, setCurrentStage, ...props}) => {
       lastX.current = clientX;
       rotationSpeed.current = delta * 0.01 * Math.PI;
     }
-  }
+  };
 
   const handleKeyDown = (e) => {
     if(e.key === 'ArrowLeft'){
       if(!isRotating) setIsRotating(true);
-      islandRef.current.rotating.y += 0.01 * Math.PI;
+      islandRef.current.rotation.y += 0.01 * Math.PI;
+      rotationSpeed.current = 0.00135;
     } else if (e.key === 'ArrowRight') {
       if(!isRotating) setIsRotating(true);
-        islandRef.current.rotating.y -= 0.01 * Math.PI;
+        islandRef.current.rotation.y -= 0.01 * Math.PI;
+        rotationSpeed.current = -0.00135;
       }
     }
 
